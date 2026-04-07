7e25e6d111b88c254430fdb94d2c95f963cbd37b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-07 19:54:57 -0300

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAv2RmuLFJyWq_J1AWYOQR2ELufbUVcY0",
+  authDomain: "fiap-auth-mobileapp.firebaseapp.com",
+  projectId: "fiap-auth-mobileapp",
+  storageBucket: "fiap-auth-mobileapp.firebasestorage.app",
+  messagingSenderId: "920985270989",
+  appId: "1:920985270989:web:cee76491d27a3c2a6a8784"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
